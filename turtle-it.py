8b69b1c2bea31dc0f7fc769aeb1fbca60b40bce1
gosha-smeh <gosha.smeh@gmail.com>
2024-10-08 02:32:12 +0300

    hello world

---
+++ turtle-it.py
@@ -0,0 +1,17 @@
+import turtle 
+
+wn = turtle.Screen() 
+player = turtle.Turtle() 
+player.shape("turtle") 
+player.goto(0,0) 
+player.left(180) 
+player.forward(80) 
+player.left(90) 
+player.forward(160) 
+player.left(90) 
+player.forward(80) 
+player.left(90) 
+player.forward(80) 
+player.left(90) 
+player.forward(80) 
+turtle.mainloop()
