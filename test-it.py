eb1f5fe194c46b55227b20340dedda85b0d5539c
Arbi <info@gebemot.com>
2024-10-08 10:54:02 +0000

    playing with git

--- test-it.py
+++ test-it.py
@@ -3,14 +3,15 @@ import os
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def printAt(y, x, str):
-    print(f'\x1b[{y};{x}H{str}')
-
 def gotoLn(y):
     print(f'\x1b[{y};0H')
+
+
+def printAt(y, x, str):
+    print(f'\x1b[{y};{x}H{str}')
 
 cls()
 
 str = "Hello, World!"
-printAt(1, 40, str)
-gotoLn(30)
+printAt(1, 30, str)
+gotoLn(10)
